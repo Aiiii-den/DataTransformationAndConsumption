19d90438e4096a2f1e638ea24db877aa0f2e45bd
annalenameyer <annalena-meyer99@web.de>
2022-11-27 12:33:03 +0100

    Verbesserung Frontend-Ordnerstruktur /2

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HintergrundbildComponent } from './components/hintergrundbild/hintergrundbild.component';
 
 @NgModule({
   declarations: [
+  
+    HintergrundbildComponent
   ],
   imports: [
     BrowserModule,
