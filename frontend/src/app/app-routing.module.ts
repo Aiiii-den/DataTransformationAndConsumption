a2fa67579047d5aeea0555a56f0d317e0f538965
Julia193 <Julia.Schultze@Student.HTW-Berlin.de>
2022-12-28 12:44:07 +0100

    US12 Annalena und Julia Code angepasst

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,24 +1,22 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
 import {TabellenseiteComponent} from "./components/tabellenseite/tabellenseite.component";
 import {StartseiteComponent} from "./components/startseite/startseite.component";
 
-
 const routes: Routes = [
- {
-        // Standardroute: Umleitung auf '/startseite'
-        path: '',
-        redirectTo: 'startseite',
-        pathMatch: 'full'
-    },
-    { path: 'startseite', component: StartseiteComponent },
-  { path: 'tabellenseite', component: TabellenseiteComponent }
-
+  {
+    // Standardroute: Umleitung auf '/startseite'
+    path: '',
+    redirectTo: 'startseite',
+    pathMatch: 'full'
+  },
+  {path: 'startseite', component: StartseiteComponent},
+  {path: 'tabellenseite', component: TabellenseiteComponent}
 ];
-
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
