86ab62c07cf40689408de06da95ca00e1398ec60
annalenameyer <annalena-meyer99@web.de>
2023-01-14 15:42:42 +0100

    US11/T11-02: Anbindung Frontend und Backend, Julia und Annalena

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,22 +1,46 @@
 import {NgModule} from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import {BrowserModule} from '@angular/platform-browser';
-
 import {AppRoutingModule} from './app-routing.module';
+
 import {AppComponent} from './app.component';
 import {TabellenseiteComponent} from './components/tabellenseite/tabellenseite.component';
 import {StartseiteComponent} from './components/startseite/startseite.component';
+
+import { GraphQLModule } from './graphql.module';
+import { ApolloModule, APOLLO_OPTIONS } from 'apollo-angular';
+import { HttpLink } from 'apollo-angular/http';
+import { InMemoryCache } from '@apollo/client/core';
+import {DigimonausgabeComponent} from "./components/digimonausgabe/digimonausgabe.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     StartseiteComponent,
-    TabellenseiteComponent
+    TabellenseiteComponent,
+    DigimonausgabeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    GraphQLModule,
+    HttpClientModule,
+    ApolloModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APOLLO_OPTIONS,
+      useFactory(httpLink: HttpLink) {
+        return {
+          cache: new InMemoryCache(),
+          link: httpLink.create({
+            uri: 'http://localhost:4000'
+          })
+        }
+      },
+      deps: [HttpLink]
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
