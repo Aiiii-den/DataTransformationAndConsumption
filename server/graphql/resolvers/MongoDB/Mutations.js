b78f556b233cf2933c82ce59657cb2f1dc7c46b2
Su Ma <mahoro-nkundineza@web,de>
2023-02-06 21:27:37 +0100

    First Error Handling (CRUD) push

--- server/graphql/resolvers/MongoDB/Mutations.js
+++ server/graphql/resolvers/MongoDB/Mutations.js
@@ -2,46 +2,57 @@ const FavoriteDBSchema = require("../../../MongoDB/FavoriteDBSchema");
 //const Favorite = FavoriteDBSchema.db.collection('FavoriteCard');
 
 //create Mutation for add favorite digimon to the FavoriteSchema
+
 const Mutations = {
-     Mutation:{
-
-        async addFavorite(parent, args, context, info){
-                const exist =  await FavoriteDBSchema.findOne({username: args.username});
-                if(exist != null){
-                    throw new Error(`${args.username} existiert bereits!`)
-                }
-                if(args.cardname == ""){
-                    throw new Error("cardname darf nicht leer sein!!")
-                }
-                const card =  new FavoriteDBSchema({
-                    username: args.username,
-                    cardname: args.cardname,
-                });
-                await card.save();
-                console.log(card);
-                return card;
-        },        
-            
-
-
-        async getAllFavorites(req, res){
-            return await FavoriteDBSchema.find({})
-        },
-
-        async updateFavoriteByUsername(parent, args){
-            return await FavoriteDBSchema.findOneAndReplace({username: args.username}, {username: args.username, cardname: args.cardname});
-        },
-
-        async deleteFavoriteById(parent, args, context, info){
-            return await FavoriteDBSchema.findOneAndDelete({_id: args._id});
-        },
-        async deleteFavotiteByUsername(parent, args){
-            return await FavoriteDBSchema.findOneAndDelete({username:args.username});
-        },
-
-        async getFavoriteByUsername(parent, args) {
-            return await FavoriteDBSchema.findOne({username: args.username});
-        },
-     }
+    Mutation:{
+       async addFavorite(parent, args, context, info){
+               const exist =  await FavoriteDBSchema.findOne({username: args.username});
+               if(exist != null){
+                   throw new Error(`${args.username} existiert bereits!`)
+               }
+               if(args.cardname == ""){
+                   throw new Error("cardname darf nicht leer sein!!")
+               }
+               const card =  new FavoriteDBSchema({
+                   username: args.username,
+                   cardname: args.cardname,
+               });
+               await card.save();
+               console.log(card);
+               return card;
+       },        async getAllFavorites(req, res){
+           return await FavoriteDBSchema.find({})
+       },        async updateFavoriteByUsername(parent, args){
+           const exist =  await FavoriteDBSchema.findOne({username: args.username});
+           console.log(exist)
+           if(exist == null){
+               throw new Error(`this id: ${args.username} existiert nicht!`)
+           }
+           if(args.cardname == ""){
+               throw new Error("cardname darf nicht leer sein!!")
+           }
+           return await FavoriteDBSchema.findOneAndReplace({username: args.username}, {username: args.username, cardname: args.cardname});
+       },        async deleteFavoriteById(parent, args, context, info){
+           const exist =  await FavoriteDBSchema.findOne({username: args._id});
+           if(exist == null){
+               throw new Error(`${args._id} existiert nicht!`)
+           }
+           return await FavoriteDBSchema.findOneAndDelete({_id: args._id});
+       },
+       async deleteFavotiteByUsername(parent, args){
+           const exist =  await FavoriteDBSchema.findOne({username: args.username});
+           if(exist == null){
+               throw new Error(`${args.username} existiert nicht!`)
+           }
+           return await FavoriteDBSchema.findOneAndDelete({username:args.username});
+       },        async getFavoriteByUsername(parent, args) {
+           const exist =  await FavoriteDBSchema.findOne({username: args.username});
+           if(exist == null){
+               throw new Error(`${args.username} existiert nicht!`)
+           }
+           return await FavoriteDBSchema.findOne({username: args.username});
+       },
+    }
 }
+
 module.exports=Mutations;
