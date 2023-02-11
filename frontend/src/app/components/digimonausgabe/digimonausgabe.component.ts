9fb57d4ea855a681e145ec0533edc574362b1c0d
annalenameyer <annalena-meyer99@web.de>
2023-02-11 12:52:56 +0100

    US20/T20-02: Frontendcode ueberarbeitet Julia,Annalena

--- frontend/src/app/components/digimonausgabe/digimonausgabe.component.ts
+++ frontend/src/app/components/digimonausgabe/digimonausgabe.component.ts
@@ -1,8 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Apollo} from "apollo-angular";
 
-
-
 @Component({
   selector: 'app-digimonausgabe',
   templateUrl: './digimonausgabe.component.html',
@@ -12,8 +10,8 @@ export class DigimonausgabeComponent implements OnInit {
 
   constructor(private apollo: Apollo) {
   }
-
   ngOnInit(): void {
+    //hier wird der name des Digimons durch die Suchleisteneingabe deklariert
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
     const value_for_name = urlParams.get('name')
@@ -38,29 +36,22 @@ export class DigimonausgabeComponent implements OnInit {
       }
     }`
 
-
-
-      fetch('http://localhost:4000/', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          query,
-          variables: {name},
-        })
+    fetch('http://localhost:4000/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        query,
+        variables: {name},
       })
-        .then(r => r.json())
-
-        .then(data => document.body.append(JSON.stringify(data)))
-
-
-
+    })
+      .then(r => r.json())
+      //Ausgabe data
+      .then(data => document.body.append(JSON.stringify(data)))
 
     document.body.style.textAlign = 'center';
     document.body.style.fontFamily = 'Chalkduster';
-    document.body.style.padding= '250px 20px 10px 10px ';
-
+    document.body.style.padding = '250px 20px 10px 10px ';
   }
-
 }
