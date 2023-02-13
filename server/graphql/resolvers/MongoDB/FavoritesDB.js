ecc2b589c9843e43f001859bf0da5734c6a3fcaa
annalenameyer <annalena-meyer99@web.de>
2023-02-13 14:00:39 +0100

    Code repariert

--- server/graphql/resolvers/MongoDB/FavoritesDB.js
+++ server/graphql/resolvers/MongoDB/FavoritesDB.js
@@ -7,36 +7,18 @@ const resolvers = {
         async getAllFavorites(){
             return await FavoriteDBSchema.find({})
         },
-        async deleteFavoriteById(parent, args){
-            const exist =  await FavoriteDBSchema.findOne({username: args._id});
-            if(exist == null){
-                return {
-                    __typename: "IDNotFound",
-                    message: `diese User mit ${args._id} existiert nicht`
-                }
-            }
-            return await FavoriteDBSchema.findOneAndDelete({_id: args._id});
-        },
-        async deleteFavotiteByUsername(parent, args){
-            const exist =  await FavoriteDBSchema.findOne({username: args.username});
-            if(exist == null){
-                return {
-                    __typename: "UserNotFound",
-                    message: `${args.username} existiert nicht`
-                }
-            }
-            return await FavoriteDBSchema.findOneAndDelete({username:args.username});
-        },
+
         async getFavoriteByUsername(parent, args) {
-            const exist =  await FavoriteDBSchema.findOne({username: args.username});
-            if(exist == null){
-                return {
-                    __typename: "UserNotFound",
-                    message: `${args.username} existiert nicht`
-                }
-            }
             return await FavoriteDBSchema.findOne({username: args.username});
         },
+
+        async deleteFavoriteById(parent, args){
+            return await FavoriteDBSchema.findOneAndDelete({_id: args._id});
+        },
+        async deleteFavoriteByUsername(parent, args){
+            return await FavoriteDBSchema.findOneAndDelete({username:args.username});
+        },
+
     }
 }
 module.exports=resolvers;
